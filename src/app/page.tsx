8c24ab7194e838870a094850f477da019efd002b
Pankajahura <pankaj.soni@ahurasense.com>
2025-12-30 11:52:59 +0530

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">
             Save and check it instantly check redeployed!testing again and again
-            Build -fourth deploy
+            Build -fifth deploy
           </li>
         </ol>
 
