cc21232b6c04cc10e818fb32ab9a882984c1290d
Pankajahura <pankaj.soni@ahurasense.com>
2026-01-04 18:44:02 +0530

    fixed:small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">
             Save and check it instantly check redeployed!testing again and again
-            Build -eleventh deploy-5
+            Build -eleventh deploy-8
             {process.env.NEXT_PUBLIC_TESTING_SECRET}
           </li>
         </ol>
